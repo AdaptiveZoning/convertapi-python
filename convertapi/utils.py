ab9a3f6f46b6f355699276b420cb06cdb577d2b2
Henry Bradlow <henrybrad@gmail.com>
2023-01-04 14:54:10 -0500

    Switch from multiprocessing to billiard.

--- convertapi/utils.py
+++ convertapi/utils.py
@@ -1,10 +1,10 @@
-import multiprocessing
+import billiard
 
 def map_in_parallel(f, values, pool_size):
     if pool_size < 2:
         return map(f, values)
 
-    pool = multiprocessing.Pool(pool_size)
+    pool = billiard.Pool(pool_size)
     results = pool.map_async(f, values)
     pool.close()
     pool.join()
